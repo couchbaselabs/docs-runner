aeab1315e0d2305c12b34889a3beefffe1d3a6bc
Hakim Cassimally <Hakim.Cassimally@gmail.com>
2026-05-13 16:52:10 +0100

    test

---
+++ test.js
@@ -0,0 +1,10 @@
+'use strict';
+import * as fs from "fs";
+import path from "node:path";
+import { promisify } from "util"
+import * as cp from "child_process"
+
+const exec = promisify(cp.exec)
+
+const { stdout } = await exec(
+    `droid exec "say hello"`)
